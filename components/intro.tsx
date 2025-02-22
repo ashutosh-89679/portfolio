9f39d5a3fc07f450208550d4b6a580eac0610f72
Ashutosh Mishra <71063368+ashutosh-89679@users.noreply.github.com>
2025-02-22 19:45:07 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -67,7 +67,7 @@ export default function Intro() {
         <span className="font-bold">Hello, I'm Ashutosh.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">3 years</span> of experience. I enjoy
-        building <span className="italic">softwares that contribute into the real worl</span>.
+        building <span className="italic">softwares that contribute to the real world</span>.
       </motion.h1>
 
       <motion.div
